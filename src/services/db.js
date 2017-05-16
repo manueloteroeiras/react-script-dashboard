f842d3401f53cd86533c468bc45f100afe24f166
Manuel Otero <manuel.otero@line64.com>
2017-05-16 17:04:59 -0300

    deploy

--- src/services/db.js
+++ src/services/db.js
@@ -1,4 +1,7 @@
 // export default "http://localhost:8080"
 // export default "http://192.168.1.2:8080"
-export default "https://sac-qa.bitflowapps.com:3002"
-// export default "https://sac-dev.bitflowapps.com:3002"
+
+// export default "https://sac-qa.bitflowapps.com:3002"
+
+
+export default "https://sac-dev.bitflowapps.com:3002"
