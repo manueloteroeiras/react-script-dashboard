0e8913075d70d656125a43a9afe765eeaa9855fa
Manuel Otero <manuel.otero@line64.com>
2017-05-17 17:26:00 -0300

    disable massive upload

--- src/services/db.js
+++ src/services/db.js
@@ -2,7 +2,7 @@
 // export default "http://192.168.1.2:8080"
 
 // PROD
-// export default "https://scholas-qa.bitflowapps.com:3002"
+export default "https://scholas-qa.bitflowapps.com:3002"
 
 // DEV
-export default "https://sac-dev.bitflowapps.com:3002"
+// export default "https://sac-dev.bitflowapps.com:3002"
